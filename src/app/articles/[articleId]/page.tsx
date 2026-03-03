7cb375584fa3e3c66ad348f2f087a788359e2418
Drew Dodds <drewdoddss@gmail.com>
2026-03-02 20:58:04 -0500

    more updates

---
+++ src/app/articles/[articleId]/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function NewsArticle({ 
+    params, 
+    searchParams,
+ }: { 
+    params: Promise<{ articleId: string }>;
+    searchParams: Promise<{lang>: "en" | "fr" | "es" }>;
+}) {
+    const { articleId } = await params;
+    const { lang = "en"} = await searchParams;
+    return (
+        <div>
+            <h1>News Article {articleId}</h1>
+            <p>Reading in language {lang}</p>
+            <div>
+                <Link href="/articles/${articleId}?lang=en">English</Link>
+                <Link href="/articles/${articleId}?lang=fr">French</Link>
+                <Link href="/articles/${articleId}?lang=es">Spanish</Link>
+            </div>
+        </div>
+    );
+}
